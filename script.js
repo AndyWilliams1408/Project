0711f2c2fb5a764abab41dbba59160493f7b4ab7
AndyWilliams1408 <134110811+AndyWilliams1408@users.noreply.github.com>
2023-08-31 21:58:33 -0500

    a
    a

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 let totalValue = 0;
 
+// Obtener el elemento del DOM por su ID
+const contenedor = document.getElementById("contenedor");
+
+// Establecer una imagen como fondo
+contenedor.style.backgroundImage = "url('fondoya.jpg')";
+
+const horaInput = document.getElementById("hora");
+
+function actualizarHora() {
+    const fechaActual = new Date();
+    const formattedHora = `${fechaActual.getHours().toString().padStart(2, '0')}:${fechaActual.getMinutes().toString().padStart(2, '0')}`;
+    horaInput.value = formattedHora;
+  }
+  // Actualizar la hora en vivo
+  actualizarHora(); // Actualizar inmediatamente al cargar la página
+  setInterval(actualizarHora, 1000);
+
+
+
+// Obtener el elemento de entrada de fecha por su ID
+const fechaInput = document.getElementById("fecha");
+
+// Crear un objeto de fecha para la fecha actual
+const fechaActual = new Date();
+
+// Restar un día a la fecha actual para obtener la fecha de ayer
+fechaActual.setDate(fechaActual.getDate() - 1);
+
+// Formatear la fecha en el formato YYYY-MM-DD
+const formattedFecha = fechaActual.toISOString().substr(0, 10);
+
+// Establecer el valor por defecto en el campo de fecha
+fechaInput.value = formattedFecha;
 
 function registrarCupo() {
     const fecha = document.getElementById("fecha").value;
@@ -8,6 +41,9 @@ function registrarCupo() {
     const recibe = document.getElementById("recibe").value;
     const pagoPor = document.getElementById("pagoPor").value;
     const pagoD = document.getElementById("pagoPor");
+
+    
+
 
     // registro de cupo
 
@@ -49,6 +85,20 @@ function registrarCupo() {
                 clientSelect.appendChild(option);
                 pagoD.value = '';
             }
+    
+
+            const valorInput = document.getElementById("valor");
+            const recibeInput = document.getElementById("recibe");
+            const pagoPorInput = document.getElementById("pagoPor");
+        
+            // Realizar las operaciones necesarias para registrar el cupo
+        
+            // Limpiar los campos después del ingreso
+
+            valorInput.value = "";
+            recibeInput.value = "";
+            pagoPorInput.value = "";
+    
 }
 
 const transaccionesTableBody = document.getElementById("transaccionesTableBody");
